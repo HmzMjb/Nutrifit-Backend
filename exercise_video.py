d2cd3ecfd0682974a02abb547b5d9652b62f4fe7
unknown <HmzMjb@users.noreply.github.com>
2026-06-24 00:42:43 +0500

    Commit

--- exercise_video.py
+++ exercise_video.py
@@ -687,8 +687,9 @@ def process_video(video_path, model_path="Models/Bench_rf.pkl"):
                 min_tracking_confidence=0.5
         ) as pose:
 
-            for image in frames:  # while loop ki jagah for loop
+            for image in frames:
                 try:
+                    image = cv2.flip(image, 1)
                     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
                     results = pose.process(image_rgb)
 
